7f881e8fd37c9fdaf51a61a887b86c291b0fd309
Althaf <althafrifath@gmail.com>
2024-01-28 12:43:48 +0530

    user role setup

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->tinyInteger('role')->default(3); // 1 = Admin, 2 = Vendor, 3 = Customer
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
