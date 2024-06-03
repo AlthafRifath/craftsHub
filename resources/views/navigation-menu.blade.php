a0c2c3f03dcce7d0d268433b6098a840d1b59622
Althaf <althafrifath@gmail.com>
2024-06-03 12:21:19 +0530

    UI changes to the Profile section

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -5,17 +5,17 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="{{ route('home') }}">
                         <x-application-mark class="block h-9 w-auto" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                </div>
+{{--                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">--}}
+{{--                    <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">--}}
+{{--                        {{ __('Dashboard') }}--}}
+{{--                    </x-nav-link>--}}
+{{--                </div>--}}
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ms-6">
@@ -70,54 +70,54 @@
                         </x-dropdown>
                     </div>
                 @endif
-                
+
                 <!-- Admin Dropdown -->
-                <div class="ms-3 relative">
-                    <x-dropdown align="right" width="48">
-                        <x-slot name="trigger">
-                            <span class="inline-flex rounded-md">
-                                <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
-                                    Adminstration
+{{--                <div class="ms-3 relative">--}}
+{{--                    <x-dropdown align="right" width="48">--}}
+{{--                        <x-slot name="trigger">--}}
+{{--                            <span class="inline-flex rounded-md">--}}
+{{--                                <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">--}}
+{{--                                    Adminstration--}}
 
-                                    <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                                    </svg>
-                                </button>
-                            </span>
-                        </x-slot>
+{{--                                    <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">--}}
+{{--                                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />--}}
+{{--                                    </svg>--}}
+{{--                                </button>--}}
+{{--                            </span>--}}
+{{--                        </x-slot>--}}
 
-                        <x-slot name="content">
-                            <!-- Account Management -->
-                            <div class="block px-4 py-2 text-xs text-gray-400">
-                                {{ __('Manage Users') }}
-                            </div>
+{{--                        <x-slot name="content">--}}
+{{--                            <!-- Account Management -->--}}
+{{--                            <div class="block px-4 py-2 text-xs text-gray-400">--}}
+{{--                                {{ __('Manage Users') }}--}}
+{{--                            </div>--}}
 
-                            <x-dropdown-link href="{{ route('user.index') }}">
-                                {{ __('Users') }}
-                            </x-dropdown-link>
+{{--                            <x-dropdown-link href="{{ route('user.index') }}">--}}
+{{--                                {{ __('Users') }}--}}
+{{--                            </x-dropdown-link>--}}
 
-                            <div class="block px-4 py-2 text-xs text-gray-400">
-                                {{ __('Manage Products') }}
-                            </div>
+{{--                            <div class="block px-4 py-2 text-xs text-gray-400">--}}
+{{--                                {{ __('Manage Products') }}--}}
+{{--                            </div>--}}
 
-                            <x-dropdown-link href="{{ route('product-category.index') }}">
-                                {{ __('Categories') }}
-                            </x-dropdown-link>
+{{--                            <x-dropdown-link href="{{ route('product-category.index') }}">--}}
+{{--                                {{ __('Categories') }}--}}
+{{--                            </x-dropdown-link>--}}
 
-                            <div class="border-t border-gray-200"></div>
+{{--                            <div class="border-t border-gray-200"></div>--}}
 
-                            <!-- Authentication -->
-                            <form method="POST" action="{{ route('logout') }}" x-data>
-                                @csrf
+{{--                            <!-- Authentication -->--}}
+{{--                            <form method="POST" action="{{ route('logout') }}" x-data>--}}
+{{--                                @csrf--}}
 
-                                <x-dropdown-link href="{{ route('logout') }}"
-                                         @click.prevent="$root.submit();">
-                                    {{ __('Log Out') }}
-                                </x-dropdown-link>
-                            </form>
-                        </x-slot>
-                    </x-dropdown>
-                </div>
+{{--                                <x-dropdown-link href="{{ route('logout') }}"--}}
+{{--                                         @click.prevent="$root.submit();">--}}
+{{--                                    {{ __('Log Out') }}--}}
+{{--                                </x-dropdown-link>--}}
+{{--                            </form>--}}
+{{--                        </x-slot>--}}
+{{--                    </x-dropdown>--}}
+{{--                </div>--}}
 
                 <!-- Settings Dropdown -->
                 <div class="ms-3 relative">
